1911adf740d1e3334b5133f619aaf5cdc4e48e54
unknown <dancesfire@gmail.com>
2021-03-13 15:46:52 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+})
+
+
+const cors = require('cors')
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(express.urlencoded({extended: false}))
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('message', (inPos) => {
+        console.log(inPos)
+        io.emit('message', inPos)
+    })
+})
+
+
+http.listen(process.env.PORT || 8000, () => {
+  console.log('Now your playing with Power.....Port 8000 Power')
+})
+
